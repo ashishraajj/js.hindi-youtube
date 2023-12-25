fbff1bebb25d595b5fbe8e1afedb214dfe08f31a
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2023-12-25 17:43:19 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,18 @@
+"use strict" //treat all the js code as newer version
+// alert(3 + 3)
+
+// number=> 2 to the power 53
+// BigInt
+// string=""
+// Boolean=> true/false
+// null=>standalon value
+// undefined=>
+// Symbol=>unique
+
+let name="ashish"
+let age=21
+let isLogged=false
+
+
+console.log(typeof "ashish");
+console.log(typeof 21);
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const userId=234533
+let userEmail="ashish@gmail.com"
+var userPassword="86889"
+userCity="delhi"
+
+// userId=398950
+userEmail="ashishay@in"
+userPassword="938930"
+userCity="mumbai"
+
+console.log(userId);
+console.table([userId, userEmail, userPassword, userCity])
