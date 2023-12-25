91c00a441518fa8ced8154ecde464e4ab362d44a
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2023-12-25 22:22:20 +0000

    converse

--- 01_basics/03_conversion_opr.js
+++ 01_basics/03_conversion_opr.js
@@ -1,28 +1,63 @@
-let score="43"
+// let score="43"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valurInNumber=Number(score)
-console.log(typeof valurInNumber);
+// let valurInNumber=Number(score)
+// console.log(typeof valurInNumber);
 
-// 33=>33
-// "123jdj"=>NaN
-// true=>1
+// // 33=>33
+// // "123jdj"=>NaN
+// // true=>1
 
-let isLoggedin=1
-let converse=Boolean(isLoggedin);
+// let isLoggedin=1
+// let converse=Boolean(isLoggedin);
 
-console.log(converse);
-console.log(typeof isLoggedin);
-
-
-// 1 => true; 0 => false
-// " " => false
-// "ashish" => true
+// console.log(converse);
+// console.log(typeof isLoggedin);
 
 
-let someNumber=44
-let cross=String(someNumber);
-console.log(cross);
-console.log(typeof cross);
+// // 1 => true; 0 => false
+// // " " => false
+// // "ashish" => true
+
+
+// let someNumber=44
+// let cross=String(someNumber);
+// console.log(cross);
+// console.log(typeof cross);
+
+
+// LECTURE-7
+let value=8
+let negValue=-value;
+console.log(value);
+console.log(negValue);
+
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3**2);
+console.log(3/2);
+
+
+let str1="hello"
+let str2="ashish"
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1=num2+num3==2 + 2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
