93313b8d23b04e262f5e0d21250a3d1ebfbfe778
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2023-12-25 20:30:08 +0000

    conversion

---
+++ 01_basics/03_conversion_opr.js
@@ -0,0 +1,27 @@
+let score="43"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valurInNumber=Number(score)
+console.log(typeof valurInNumber);
+
+// 33=>33
+// "123jdj"=>NaN
+// true=>1
+
+let isLoggedin=1
+let converse=Boolean(isLoggedin);
+
+console.log(converse);
+console.log(typeof isLoggedin);
+
+
+// 1 => true; 0 => false
+// " " => false
+// "ashish" => true
+
+
+let someNumber=44
+let cross=String(someNumber);
+
