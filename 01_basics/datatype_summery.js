c3eb0d2d0d4f552129b2c54f9d839b8209861132
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2023-12-26 12:31:29 +0000

    datatype

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,36 @@
+//primitive
+
+// string , Number, boolean, Null, undefined, symbol, bigint
+
+//Refrensh(Non-primitive)
+
+const score=8
+const scorevalue=34.3
+
+const isLoggedin=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid);
+
+
+//this is how we represent bigint
+const bigNumber=83898305042974820n
+
+//array
+const heros=["saktiman", "nagraj", "doga"]
+
+
+//object will be always in curly brackets
+let me={
+    name: "ashish",
+    age: 21,
+}
+
+// const myFunction= funtion(){
+//     console.log("hello world");
+// }
+
+console.log(typeof heros);
