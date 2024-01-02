7a55b71052ab9633a2d895efd8d79dcb6cafc165
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2024-01-02 10:39:37 +0000

    for

---
+++ iteration/one.js
@@ -0,0 +1,24 @@
+//FOR LOOP
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i < 6; i++) {
+//     for (let j = 0; j < 6; j++) {
+        
+//         console.log(i+'*'+j+'='+i*j);
+//     }
+// }
+
+
+//BREAK AND CONTINIOU
+for (let index = 0; index <= 20; index++) {
+    if (index=5) {
+        console.log("detection 5");
+        break
+    }
+    console.log(`value of i is ${index}`);
+    
+}
