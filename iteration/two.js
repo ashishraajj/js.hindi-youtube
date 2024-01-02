152807fed690fd0787b6888339ff1b09724b1f38
ASHISH RAJ <123008427+ashishraajj@users.noreply.github.com>
2024-01-02 10:47:20 +0000

    two

---
+++ iteration/two.js
@@ -0,0 +1,14 @@
+//WHILE AND DO WHILE LOOP
+
+let index=0
+while (index<=10) {
+    index++
+    console.log(index);
+}
+
+//do while
+let i=0
+do {
+    console.log(`the value is ${i}`);
+    i++
+} while (i<=10);
